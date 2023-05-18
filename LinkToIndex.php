a0665478edc98c9646c95e6379f0732edc8a3a9a
ewaldo95 <ewaldo.fischer-alpire@informatik.hs-fulda.degit config --global user.name EWALDO>
2023-05-18 18:02:16 +0200

    Changes for text

---
+++ LinkToIndex.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Heer you go to Index</title>
+
+
+    </head>
+    <body>
+        <h1> Go to Index </h1>
+        <br>
+        <a href="index.php" target="_blank">go to Index!!!</a>
+        <br>
+        <span style="margin-right: 20px;">Texto antes</span>Texto después
+
+        <h1> Has visto el perfil de Ewaldo? :) </h1>
+
+
+        <a href="https://ewaldo-fa.wixsite.com/ewaldo-fischer"> Ewaldo Fischer Porfolio!!!</a>
+
+    </body>
+
+
+
+</html>
